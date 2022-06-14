fd7fe0b0c4477a454f10116773910ea2634de168
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-06-14 23:30:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,3 +15,8 @@ server_scripts {
 	'config.lua',
 	'server/*.lua'
 }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3 '
+vorp_github 'https://github.com/VORPCORE/vorp_Crawfish'
