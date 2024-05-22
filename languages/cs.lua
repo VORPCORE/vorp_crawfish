ab0c733484bde47004c3ef723e2791c3c50cbf5f
zdenda142 <77118689+zdenda142@users.noreply.github.com>
2024-05-22 22:10:34 +0200

    add czech translate

---
+++ languages/cs.lua
@@ -0,0 +1,15 @@
+
+Locales["cs"] = {
+    item_crawfish = "Rak",
+    item_harvest = "Syrové korýšové maso",
+    hole = "Díra s raky",
+    search_hole = "Hledat díru s raky",
+    inv_nospace = "Nedostatek místa ve vašem inventáři!",
+    search_current = "Tato díra s raky je právě vyhledávána!",
+    search_recent = "Tato díra s raky byla nedávno vyhledána...",
+    searching = "Probíhá vyhledávání díry s raky...",
+    search_found = "Našli jste {count} x {item}!",
+    harvesting = "Sklízení...",
+    harvested = "Sklidili jste {count} x {item}!"
+}
+
