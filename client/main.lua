00fde02894eebf577695613f00d1cae167b994cf
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-06-01 14:42:53 +0100

    refactor: use of new exports and some clean up

--- client/main.lua
+++ client/main.lua
@@ -1,55 +1,63 @@
-
-RegisterNetEvent("vorp_crawfish:try_search")
-RegisterNetEvent("vorp_crawfish:do_search")
-RegisterNetEvent("vorp_crawfish:abort_search")
-RegisterNetEvent("vorp_crawfish:harvest")
-
-local _prompts, _prompt = GetRandomIntInRange(0, 0xffffff), nil
 local searching, showprompt, nearest = false, false, 0
 
-AddEventHandler("vorp_crawfish:try_search",function()
+RegisterNetEvent("vorp_crawfish:try_search", function()
 	searching, nearest = false, 0
 end)
 
-AddEventHandler("vorp_crawfish:do_search",function(holeIndex, searchTime)
+RegisterNetEvent("vorp_crawfish:do_search", function(holeIndex, searchTime)
 	local playerPed = PlayerPedId()
-	TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), searchTime, true, false, false, false)
+	TaskStartScenarioInPlaceHash(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), searchTime, true, "", 0, false)
 	exports['progressBars']:startUI(searchTime, _U("searching"))
-	Citizen.Wait(searchTime)
+	Wait(searchTime)
 	ClearPedTasksImmediately(playerPed)
 	nearest = 0
 	searching = false
-	TriggerServerEvent("vorp_crawfish:do_search",holeIndex)
+	TriggerServerEvent("vorp_crawfish:do_search", holeIndex)
 end)
 
-AddEventHandler("vorp_crawfish:harvest",function()
+RegisterNetEvent("vorp_crawfish:harvest", function()
 	local playerPed = PlayerPedId()
 	local dict, anim = "mech_skin@chicken@field_dress", "success"
 	RequestAnimDict(dict)
 	while not HasAnimDictLoaded(dict) do
-		Wait(0) 
+		Wait(0)
 	end
-	TaskPlayAnim(playerPed, dict, anim, 1.0, 1.0, 4000, 16, 0.0, false, false, false, '', false)
+	TaskPlayAnim(playerPed, dict, anim, 1.0, 1.0, 4000, 16, 0.0, false, 0, false, '', false)
 	exports['progressBars']:startUI(5000, _U("harvesting"))
-	Citizen.Wait(5000)
+	Wait(5000)
 	ClearPedTasksImmediately(playerPed)
 	RemoveAnimDict(dict)
 	TriggerServerEvent("vorp_crawfish:harvest")
 end)
 
-Citizen.CreateThread(function()
+function RegisterPrompts()
+	local _promptGroup = GetRandomIntInRange(0, 0xffffff)
+	local _prompt = UiPromptRegisterBegin()
+	UiPromptSetControlAction(_prompt, 0x760A9C6F) -- G key
+	local str = CreateVarString(10, 'LITERAL_STRING', _U("hole"))
+	UiPromptSetText(_prompt, str)
+	UiPromptSetEnabled(_prompt, true)
+	UiPromptSetStandardMode(_prompt, true)
+	UiPromptSetGroup(_prompt, _promptGroup, 0)
+	UiPromptRegisterEnd(_prompt)
+	return _prompt, _promptGroup
+end
+
+CreateThread(function()
+	repeat Wait(2000) until LocalPlayer.state.IsInSession
+	local _prompt, _promptGroup = RegisterPrompts()
+
 	while true do
-		Citizen.Wait(0)
 		showprompt = false
-		local sleep = true
+		local sleep = 1000
 		local pedID = PlayerPedId()
-		local DeadOrDying = IsPedDeadOrDying(pedID)
+		local DeadOrDying = IsPedDeadOrDying(pedID, false)
 		if (not searching) and (not DeadOrDying) then
 			local coords = GetEntityCoords(pedID)
 			for index, pos in ipairs(Config.CrawfishHoles) do
 				local distance = #(coords - pos)
 				if distance <= 1.5 then
-					sleep = false 
+					sleep = 0
 					showprompt = true
 					nearest = index
 					break
@@ -61,26 +69,17 @@ Citizen.CreateThread(function()
 			TriggerServerEvent("vorp_crawfish:abort_search")
 		end
 		if showprompt and (not searching) and (nearest > 0) and (not DeadOrDying) then
-			PromptSetActiveGroupThisFrame(_prompts, CreateVarString(10, 'LITERAL_STRING', _U("search_hole")))
-			if Citizen.InvokeNative(0xC92AC953F0A982AE,_prompt) then
-				sleep = true
+			sleep = 0
+			local label = CreateVarString(10, 'LITERAL_STRING', _U("search_hole"))
+			UiPromptSetActiveGroupThisFrame(_promptGroup, label, 0, 0, 0, 0)
+
+			if UiPromptHasStandardModeCompleted(_prompt, 0) then
 				searching = true
-				TriggerServerEvent("vorp_crawfish:try_search",nearest)
+				TriggerServerEvent("vorp_crawfish:try_search", nearest)
 			end
 		end
 		nearest = 0
-		if sleep then Citizen.Wait(500) end
-	end
-end)
 
-Citizen.CreateThread(function()
-	_prompt = PromptRegisterBegin()
-	PromptSetControlAction(_prompt, 0x760A9C6F) -- G key
-	local str = CreateVarString(10, 'LITERAL_STRING', _U("hole"))
-	PromptSetText(_prompt, str)
-	PromptSetEnabled(_prompt, 1)
-	PromptSetStandardMode(_prompt,1)
-	PromptSetGroup(_prompt, _prompts)
-	Citizen.InvokeNative(0xC5F428EE08FA7F2C,_prompt,true)
-	PromptRegisterEnd(_prompt)
+		Wait(sleep)
+	end
 end)
